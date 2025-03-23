d83ebd5792e89be6b5da703c73e0855267cf39d4
NellowTCS <neeljaiswal23@gmail.com>
2025-03-23 17:48:33 -0600

    Create Main.cpp

---
+++ App/Source/Main.cpp
@@ -0,0 +1,33 @@
+#include "Boards.h"
+#include <Tactility/Tactility.h>
+
+#ifdef ESP_PLATFORM
+#include "tt_init.h"
+#endif
+
+extern const tt::app::AppManifest touch_calibration_app;
+
+
+extern "C" {
+
+void app_main() {
+    static const tt::Configuration config = {
+        /**
+         * Auto-select a board based on the ./sdkconfig.board.* file
+         * that you copied to ./sdkconfig before you opened this project.
+         */
+        .hardware = TT_BOARD_HARDWARE,
+        .apps = {
+            &touch_calibration_app,
+
+        }
+    };
+
+#ifdef ESP_PLATFORM
+    tt_init_tactility_c(); // ELF bindings for side-loading on ESP32
+#endif
+
+    tt::run(config);
+}
+
+} // extern
